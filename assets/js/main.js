1a918705158916f3dbb011e802c28fe531b2f2cc
Madaminov <muhammadanasxon6694@gmail.com>
2022-11-27 22:43:23 +0500

    ADD

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,20 +16,6 @@ if(navToggle){
 if(navClose){
     navClose.addEventListener('click', () =>{
     })
-}
-
-/*=============== REMOVE MENU MOBILE ===============*/
-const navLink = document.querySelectorAll('.nav__link')
-
-function linkAction(){
-    const navMenu = document.getElementById('nav-menu')
-    // When we click on each nav__link, we remove the show-menu class
-    navMenu.classList.remove('show-menu')
-}
-navLink.forEach(n => n.addEventListener('click', linkAction))
-
-/*=============== CHANGE BACKGROUND HEADER ===============*/
-const scrollHeader = () =>{
     const header = document.getElementById('header')
     // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
     this.scrollY >= 50 ? header.classList.add('scroll-header') 
