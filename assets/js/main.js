cfb098a8cdb32c1d972f877b17a40e4e81fcdf00
Madaminov <muhammadanasxon6694@gmail.com>
2022-11-27 22:44:18 +0500

    ADD

--- assets/js/main.js
+++ assets/js/main.js
@@ -70,18 +70,7 @@ let newSwiper = new Swiper(".new-swiper", {
 const sections = document.querySelectorAll('section[id]')
     
 
-window.addEventListener('scroll', scrollActive)
-
-/*=============== SHOW SCROLL UP ===============*/ 
-const scrollUp = () =>{
-	const scrollUp = document.getElementById('scroll-up')
-    // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
-	this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
-						: scrollUp.classList.remove('show-scroll')
-}
-window.addEventListener('scroll', scrollUp)
-
-/*=============== SHOW CART ===============*/
+w
 const cart = document.getElementById('cart'),
       cartShop = document.getElementById('cart-shop'),
       cartClose = document.getElementById('cart-close')
